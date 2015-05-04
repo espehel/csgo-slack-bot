0ec5eb624534cbfd406235d0eb8c1b66a3f7fe02
espehel <espenhellerud5@gmail.com>
2015-05-04 12:28:13 +0200

    using json to store the config

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = require('./config.json');
+
+
+module.exports.ircConfig = config.ircConfig;
+
+module.exports.steamApiKey = config.steamApiKey;
+
+module.exports.slackToken = config.slackToken;
+
+module.exports.databasePath = config.databasePath;
+
+module.exports.slackChannels = config.slackChannels;
